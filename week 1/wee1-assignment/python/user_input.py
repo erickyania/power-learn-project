0501c57cbbc1e6e46c85942e393f0775eb28a385
eric kyania <erickyania0@gmail.com>
2024-02-22 14:05:37 +0300

    first commit

---
+++ week 1/wee1-assignment/python/user_input.py
@@ -0,0 +1,6 @@
+name = input("Enter your name\n")
+age = int(input("enter your age\n"))
+location = input("Enter your location")
+
+
+print("hello",name, "you are", age, "years old and live in in",location)
