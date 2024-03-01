7ba037e151be36863d173f3547354e78bf159e2b
eric kyania <erickyania0@gmail.com>
2024-03-01 12:55:40 +0300

    dart assignment week 2

---
+++ week/week2-assignment/dart/assignment1.dart
@@ -0,0 +1,21 @@
+void main() {
+  // Integer (int)
+  int age = 30; // Stores whole numbers
+  print("My age is: $age");
+
+  // Double (double)
+  double pi = 3.14159; // Stores decimal numbers
+  print("The value of pi is: $pi");
+
+  // String (String)
+  String name = "Alice"; // Stores sequences of characters
+  print("My name is: $name");
+
+  // List (List<T>)
+  List<String> fruits = ["apple", "banana", "orange"]; // Stores ordered collections of elements
+  print("My favorite fruits are: $fruits");
+
+  // Map (Map<K, V>)
+  Map<String, String> person = {"name": "Bob", "age": "25"}; // Stores key-value pairs
+  print("Person's information: $person");
+}
